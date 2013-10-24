4af76e1ef0c4467830853f202cda751b11b68c7c
bonet <bonetsu@gmail.com>
2013-10-25 01:23:31 +0700

    Added CategoriesPerPublisher model + test to replace PubCatNamelist model

---
+++ app/models/categories_per_publisher.rb
@@ -0,0 +1,37 @@
+# CategoriesPerPublisher list can be used for navigation menu
+
+class CategoriesPerPublisher
+  
+  include Mongoid::Document
+  
+  field :namelist
+  
+  def self.cron_update_categories_per_publisher
+
+    namelist = {}
+    
+    self.delete_all # reset the list
+
+    Publisher.all.each do |publisher|
+      
+      Newsfeed.where(:publisher_id => publisher._id).each do |newsfeed|
+        
+        if namelist[publisher._id].nil?
+          namelist[publisher._id]  = {  :publisher_id => publisher._id, 
+                                        :publisher_name => publisher.name, 
+                                        :categories => []
+                                     }
+        end
+
+        namelist[publisher._id][:categories] << {  :newsfeed_id => newsfeed._id, 
+                                                   :category_id => newsfeed.category_id, 
+                                                   :category_name => Category.find(newsfeed.category_id).name
+                                                }
+      end
+      
+    end
+
+    self.create(:namelist => namelist)
+
+  end
+end
---
+++ spec/models/categories_per_publisher_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe CategoriesPerPublisher do
+  
+  context "Class definition" do
+    it { CategoriesPerPublisher.should respond_to(:cron_update_categories_per_publisher) } 
+  end
+  
+  context "Cron test" do
+    
+    before do
+      FactoryGirl.create(:nyt_tech_rss_newsfeed)
+      FactoryGirl.create(:nyt_arts_rss_newsfeed)
+      CategoriesPerPublisher.cron_update_categories_per_publisher
+    end
+    
+    it "should create Categories per Publisher list" do
+      CategoriesPerPublisher.count.should eql(1)
+    end
+    
+    it "should have a complete namelist" do
+      cats_per_publisher = CategoriesPerPublisher.first.namelist["2"]
+      
+      cats_per_publisher.should_not be_empty
+      cats_per_publisher["publisher_id"].should be > 0
+      cats_per_publisher["publisher_name"].should_not be_empty
+      cats_per_publisher["categories"][0]["newsfeed_id"].should be > 0
+      cats_per_publisher["categories"][0]["category_id"].should be > 0
+      cats_per_publisher["categories"][0]["category_name"].should_not be_empty
+    end
+  end
+end
